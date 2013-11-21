5219e8d075b278b185505ad56b12ebefb26a41b6
Will Fisher <will@teknofire.net>
2013-11-21 00:18:35 -0900

    search on words in scene_gid

--- app/controllers/scraps_controller.rb
+++ app/controllers/scraps_controller.rb
@@ -4,7 +4,9 @@ class ScrapsController < ApplicationController
   def index
     @scraps = Scrap.order('scraps.updated_at DESC')
     @scraps = @scraps.includes(:footprint, :license)
-    @scraps = @scraps.where('footprints.scene_gid ilike ?', "%#{params[:scene_gid]}%")
+    params[:scene_gid].split(' ').each do |word|
+      @scraps = @scraps.where('footprints.scene_gid ilike ?', "%#{word}%")
+    end
     @scraps = @scraps.where('licenses.downloadable = ?', true) if params[:public]
     @scraps = @scraps.where('licenses.id = ?', params[:license_id]) if params[:license_id].present?
     @scraps = @scraps.paginate(page: params[:page])
