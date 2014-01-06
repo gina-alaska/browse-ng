93f199d4732a10a59176fcf45d8e130097209372
Will Fisher <will@teknofire.net>
2014-01-06 14:55:35 -0900

    create the database and add the user with permissions

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :format, :pretty
 # set :pty, true
 
 set :linked_files, %w{config/database.yml config/initializers/secret_token.rb}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle vendor/assets/bower_components public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
